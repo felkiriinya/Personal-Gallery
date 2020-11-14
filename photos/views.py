98cfe33b3bc1707528de9c9122b68e22d4cf9b56
felkiriinya <felkiriinya@gmail.com>
2020-11-14 04:29:44 +0300

    images added can be viewed on the home page

--- photos/views.py
+++ photos/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render,redirect
 from django.http  import HttpResponse,Http404
-
+from .models import Image,Location,Category
 # Create your views here.
 
 def photos(request):
-    return render(request, 'all-photos/all_photos.html')
+    images = Image.get_images()
+    locations = Location.objects.all()
+    categories = Category.objects.all()
+    return render(request, 'all-photos/all_photos.html',{"images":images,"locations":locations,"categories":categories})
